799f0b9365b09aa2cb8bda622ec6467ee50954df
cwalsh <cwalsh@leapfrogonline.com>
2015-09-10 16:52:51 -0500

    adds category and page models, population script, and customizes the admin

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+
+class AdminPage(admin.ModelAdmin):
+    
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category)
+admin.site.register(Page, AdminPage)
+
