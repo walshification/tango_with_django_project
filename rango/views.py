1515bd344340ccf8d85bebd71ae56edb910711af
cwalsh <cwalsh@leapfrogonline.com>
2015-09-14 16:14:11 -0500

    creates add_page view and url

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from rango.models import Category, Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -27,6 +27,7 @@ def category(request, category_name_slug):
         pages = Page.objects.filter(category=category)
         context['pages'] = pages
         context['category'] = category
+        context['category_name_slug'] = category_name_slug
     except Category.DoesNotExist:
         pass
 
@@ -47,3 +48,29 @@ def add_category(request):
         form = CategoryForm()
 
     return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == 'Post':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context = {'form': form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context)
